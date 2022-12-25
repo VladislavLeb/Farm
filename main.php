2d2ce2811c46303d4b10960c5d5a6b120d3a353d
VladislavLeb <61500530+VladislavLeb@users.noreply.github.com>
2022-12-25 16:51:50 +0300

    Create Farm backend

---
+++ main.php
@@ -0,0 +1,170 @@
+<?php
+abstract class Animal
+{
+    abstract public function getId();
+    
+    abstract public function getType();
+    
+    abstract public function collectProducts();
+}
+
+class Cow extends Animal
+{
+    private $id;
+    private $type;
+    private $maxAmount;
+    private $minAmount;
+    
+    function __construct($type)
+    { 
+        $this->id = uniqid();
+        $this->type = $type;
+        $this->minAmount = 8;
+        $this->maxAmount = 12;
+    }
+    
+    function getId()
+    {
+    	return $this->id;
+    }
+    
+    function getType()
+    {
+    	return $this->type;
+    }
+    
+    function collectProducts()
+    {
+    	return ["name" => "milk", "amount" => rand($this->minAmount, $this->maxAmount)];
+    }
+    
+}
+
+class Chicken extends Animal
+{
+	private $id;
+    private $type;
+    private $maxAmount;
+    private $minAmount;
+    
+    function __construct($type)
+    { 
+        $this->id = uniqid();
+        $this->type = $type;
+        $this->minAmount = 0;
+        $this->maxAmount = 1;
+    }
+    
+    function getId()
+    {
+    	return $this->id;
+    }
+    
+    function getType()
+    {
+    	return $this->type;
+    }
+    
+    function collectProducts()
+    {
+    	return ["name" => "egg", "amount" => rand($this->minAmount, $this->maxAmount)];
+    }
+
+}
+
+class AnimalFactory
+{
+	public function create($type)
+	{
+		switch ($type) {
+			case 'Cow':
+				return new Cow($type);
+			case 'Chicken':
+				return new Chicken($type);
+			default:
+				throw new Exception('Cant find animal type='.$type);
+		}
+	}
+}
+
+class Farm
+{
+	private $animalFactory;
+	private $animals = [];
+	private $animalTypes = [];
+
+	function __construct()
+    { 
+        $this->animalFactory = new AnimalFactory();
+    }
+    
+	public function addAnimal($type) // create and store animal
+	{
+		array_push($this->animals, $this->animalFactory->create($type));
+		if (isset($this->animalTypes[$type])) {
+			$this->animalTypes[$type]++;
+		} else {
+			$this->animalTypes[$type] = 1;
+		}
+	}
+	
+	public function getAllAnimals() // get list of all animals on farm
+	{
+		print("List of all animals:\n");
+		foreach ($this->animals as $animal) {
+			print('Animal of type '.$animal->getType().' has unique id='.$animal->getId()."\n");
+		}
+		print('Total:'.count($this->animals));
+	}
+	
+	public function getAnimalsGrouped() // get amount of animals grouped by type
+	{
+		print("List of all animals:\n");
+		print_r($this->animalTypes);
+	}
+	
+	public function collectProducts($days) // collect products from animals
+	{
+		$collectedProducts = [];
+		
+		for ($i = 0; $i < $days; $i++) {
+			foreach ($this->animals as $animal) {
+				$animalProduct = $animal->collectProducts();
+				// print_r('Collect '.$animalProduct['name'].' with amount '.$animalProduct['amount']."\n"); // uncomment to see products from every animal
+				if (isset($collectedProducts[$animalProduct['name']])) {
+					$collectedProducts[$animalProduct['name']] += $animalProduct['amount'];
+				} else {
+					$collectedProducts[$animalProduct['name']] = $animalProduct['amount'];
+				}
+			}
+		}
+		print("Collected products for ".$days." days:\n");
+		print_r($collectedProducts);
+	}
+}
+
+$week = 7;
+
+$farm = new Farm;
+
+for ($i = 0; $i < 10; $i++) {
+	$farm->addAnimal('Cow');
+}
+
+for ($i = 0; $i < 20; $i++) {
+	$farm->addAnimal('Chicken');
+}
+
+$farm->getAnimalsGrouped();
+
+$farm->collectProducts($week);
+
+$farm->addAnimal('Cow');
+
+for ($i = 0; $i < 5; $i++) {
+	$farm->addAnimal('Chicken');
+}
+
+$farm->getAnimalsGrouped();
+
+$farm->collectProducts($week);
